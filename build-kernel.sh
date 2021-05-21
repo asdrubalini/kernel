5541069b6fff2c5e55d242f5bcf1d62fe5fec484
Giovanni <indirizzoemail@mail.com>
2021-05-21 20:56:20 +0200

    add build script

---
+++ build-kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $1 \
+    && make -j17 \
+    && make modules \
+    && sudo make modules_install \
+    && sudo cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux \
+    && sudo mkinitcpio -P linux \
+    && paru -S nvidia-beta-dkms \
+    && paru -S virtualbox-host-dkms \
+    && sudo grub-mkconfig -o /boot/grub/grub.cfg
